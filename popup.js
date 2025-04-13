422068437aa748627bd1fb56c30fc093ffcdb28f
rlop666 <rlop666@gmail.com>
2025-04-13 11:01:33 +0200

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('exportBtn').addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"]
+  });
+});
